c7a0850fdc9599bbd463cffaed21fd6a6f5a99f8
anupongpk <anupong.p@aware.co.th>
2024-05-03 15:57:20 +0700

    finish ed navbar component

--- navbar-component/js/index.js
+++ navbar-component/js/index.js
@@ -1,2 +1,9 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+const menuToggleEl = document.querySelector(".nav__menu-toggle");
+const navMobileEl = document.querySelector(".nav__mobile");
+const headerEl = document.querySelector(".header");
+
+menuToggleEl.addEventListener("click", () => {
+  menuToggleEl.classList.toggle("nav__menu-toggle--open");
+  navMobileEl.classList.toggle("nav__mobile--open");
+  headerEl.classList.toggle("bg-white");
+});
