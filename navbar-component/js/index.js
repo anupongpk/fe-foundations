a2b69d3da96775c4fbaa8be262d305e4896502a7
anupongpk <anupong.p@aware.co.th>
2024-05-03 22:07:41 +0700

    refactor codes about ARIA attributes and trigger dismissing menu

--- navbar-component/js/index.js
+++ navbar-component/js/index.js
@@ -2,8 +2,32 @@ const menuToggleEl = document.querySelector(".nav__menu-toggle");
 const navMobileEl = document.querySelector(".nav__mobile");
 const headerEl = document.querySelector(".header");
 
+// toggle aria attributes
+function toggleAriaAttributes() {
+  navMobileEl.ariaExpanded =
+    navMobileEl.ariaExpanded === "true" ? "false" : "true";
+  navMobileEl.ariaHidden = navMobileEl.ariaHidden === "true" ? "false" : "true";
+}
+
 menuToggleEl.addEventListener("click", () => {
   menuToggleEl.classList.toggle("nav__menu-toggle--open");
   navMobileEl.classList.toggle("nav__mobile--open");
   headerEl.classList.toggle("bg-white");
+  toggleAriaAttributes();
+  menuToggleEl.blur();
+});
+
+// Escape key to close mobile menu
+function CloseMobileMenu() {
+  menuToggleEl.classList.remove("nav__menu-toggle--open");
+  navMobileEl.classList.remove("nav__mobile--open");
+  headerEl.classList.remove("bg-white");
+  menuToggleEl.focus();
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    CloseMobileMenu();
+    toggleAriaAttributes();
+  }
 });
